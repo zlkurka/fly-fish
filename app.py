0c6340ed5ec24188428543d386d0a9d17e6826cc
zlkurka <kurkaz4709@gmail.com>
2025-12-28 11:42:08 -0600

    Added Textual template file

---
+++ app.py
@@ -0,0 +1,16 @@
+from textual.app import App
+from textual.widgets import Label, Static
+
+from fly_fishing_sim import main, go_fishing, market, merchant
+
+class HelloTextualApp(App):
+    def compose(self):
+        self.static = Static("Welcome to FLY FISHING SIM!")
+        yield self.static
+
+        self.label = Label("Click here to go fishing.")
+        yield self.label
+
+if __name__ == "__main__":
+    app = HelloTextualApp()
+    app.run()
